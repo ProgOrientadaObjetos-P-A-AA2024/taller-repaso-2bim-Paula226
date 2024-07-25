737080d5e265e9363f3392d015f4ab8d9ffdb5eb
paula226 <palopez25@utpl.edu.ec>
2024-07-24 19:24:51 -0500

    segunda version

---
+++ TipoSuv/src/modelo/TipoCamioneta.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import cliente.Comprador;
+
+/**
+ *
+ * @author Paula Lopez
+ */
+public class TipoCamioneta extends Comprador {
+    private String marca;
+    private double precioBase;
+    private double precioFinal;
+    private double porcentajeFijo;
+    private double seguroAdicional;
+    private double valorAdicional;
+
+    public TipoCamioneta(String mar, double preBase, double porFijo,
+            String nom, String ced) {
+        super(nom, ced);
+        marca = mar;
+        precioBase = preBase;
+        porcentajeFijo = porFijo;
+    }
+
+    public void establecerMarca(String n) {
+        marca = n;
+    }
+
+    public void establecerPrecioBase(double n) {
+        precioBase = n;
+    }
+
+    public void establecerPrecioFinal() {
+        precioFinal = precioBase + porcentajeFijo + seguroAdicional;
+    }
+
+    public void establecerPorcentajeFijo(double n) {
+        porcentajeFijo = n;
+    }
+
+    public void establecerSeguroAdicional() {
+        seguroAdicional = valorAdicional * 1.5;
+    }
+
+    public void establecerValorAdicional() {
+        valorAdicional = precioBase * (porcentajeFijo/100);
+    }
+
+    public String obtenerMarca() {
+        return marca;
+    }
+
+    public double obtenerPrecioBase() {
+        return precioBase;
+    }
+
+    public double obtenerPrecioFinal() {
+        return precioFinal;
+    }
+
+    public double obtenerPorcentajeFijo() {
+        return porcentajeFijo;
+    }
+
+    public double obtenerSeguroAdicional() {
+        return seguroAdicional;
+    }
+
+    public double obtenerValorAdicional() {
+        return valorAdicional;
+    }
+    @Override
+    public String toString() {
+        String cadena = String.format( "%sTipo Camioneta:\n"
+        + "Marca: %s\n"
+        + "Precio Base: %.2f\n"
+        + "Precio Final: %.2f\n"
+        + "Porcentaje Fijo: %d\n"
+        + "Seguro Adicional: %.2f\n"
+        + "Valor Adicional: %.2f\n",
+                super.toString(), marca,precioBase, precioFinal, 
+                porcentajeFijo,seguroAdicional,valorAdicional);
+        return cadena;
+    }
+    
+}
--- TipoSuv/src/modelo/TipoSedan.java
+++ TipoSuv/src/modelo/TipoSedan.java
@@ -49,6 +49,41 @@ public class TipoSedan extends Comprador {
     public void establecerSeguroMecanico(double seguroMecanico) {
         seguroMecanico = precioBase * 0.01;
     }
-    
-    
+
+    public String obtenerMarca() {
+        return marca;
+    }
+
+    public double obtenerPrecioBase() {
+        return precioBase;
+    }
+
+    public double obtenerPrecioFinal() {
+        return precioFinal;
+    }
+
+    public double obtenerPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    public double obtenerValorDescuento() {
+        return valorDescuento;
+    }
+
+    public double obtenerSeguroMecanico() {
+        return seguroMecanico;
+    }
+    @Override
+    public String toString() {
+        String cadena = String.format( "%sTipo Sedan:\n"
+        + "Marca: %s\n"
+        + "Precio Base: %.2f\n"
+        + "Precio Final: %.2f\n"
+        + "Porcentaje Descuento: %d\n"
+        + "Valor del Descuento: %.2f\n"
+        + "Seguro Mecanico: %.2f\n",
+    super.toString(), marca,precioBase, precioFinal, 
+    porcentajeDescuento, valorDescuento, seguroMecanico);
+        return cadena;
+    }
 }
